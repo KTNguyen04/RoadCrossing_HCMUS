dba4f8a24b461574d72a62e8e9440eea5b073f08
HuyHuhHuh <126313050+HuyHuhHuh@users.noreply.github.com>
2023-10-17 21:09:24 +0700

    Update CCar.cpp

--- MainProgram/CCar.cpp
+++ MainProgram/CCar.cpp
@@ -44,10 +44,14 @@ void CCar::drawObject(int color)
 	if (fromX <= CGame::getCoorTopLeftX()) 
 		fromX = CGame::getCoorTopLeftX() + 1;
 	
-	CConsole::drawHorLine(fromX, toX, coorY * 2, 220, color);
-	CConsole::drawHorLine(fromX, toX, coorY * 2 - height, 220, color);
-	CConsole::drawVerLine(coorY - height / 2, coorY, fromX, 220, color);
-	CConsole::drawVerLine(coorY - height / 2, coorY, toX, 220, color);
+	
+	CConsole::drawHorLine(fromX, toX-6, coorY * 2, 223, color);
+	CConsole::drawHorLine(fromX+5, toX-1, coorY * 2 , 223, color);
+	color = Blue;
+	CConsole::drawHorLine(fromX-1, toX - 6, coorY * 2 - height+1, 219, color);
+	CConsole::drawHorLine(toX-5, toX -1, coorY * 2 - height + 1, 220, color);
+	CConsole::drawHorLine(toX , toX + 1, coorY * 2 - height + 1, 219, color);
+	CConsole::drawHorLine(fromX, fromX+6, coorY * 2 - height , 220, color);
 }
 
 
