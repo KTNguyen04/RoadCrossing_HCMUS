6cd6ec6d507da284077895a7fbb2a773aee2e389
HuyHuhHuh <truongnhathuy777@gmail.com>
2023-11-24 22:56:22 +0700

    Cập nhật

--- MainProgram/CGame.cpp
+++ MainProgram/CGame.cpp
@@ -871,7 +871,7 @@ void CGame::deadPopUp()
 }
 
 string CGame::chooseSavedFile() {
-	CConsole::clearScreen();
+	CConsole::clearScreen(White);
 	CConsole::drawHorLine(68 + 20, 93 + 20, 21 - 15, topBlock, 4, 15);
 	CConsole::drawChar(67 + 20, 21 - 15, botBlock, 4, 15);
 	CConsole::drawChar(94 + 20, 21 - 15, botBlock, 4, 15);
